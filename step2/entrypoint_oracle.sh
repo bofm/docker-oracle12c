9f2735d2cc1ff299c76e1bdfd7d378d0a8ff5008
bofm <bofm@noemail.com>
2015-07-20 18:07:55 +0300

    Refactoring

--- step2/entrypoint_oracle.sh
+++ step2/entrypoint_oracle.sh
@@ -7,12 +7,18 @@ alert_log="$ORACLE_DATA/diag/rdbms/orcl/$ORACLE_SID/trace/alert_$ORACLE_SID.log"
 listener_log="$ORACLE_BASE/diag/tnslsnr/$HOSTNAME/listener/trace/listener.log"
 pfile=$ORACLE_DATA/dbs/init$ORACLE_SID.ora
 
+# monitor $logfile
+monitor() {
+    tail -F -n 0 $1 | while read line; do echo -e "$2: $line"; done
+}
+
+
 if [ "$1" = 'listener' ]; then
 
 	trap "echo_red 'Caught SIGTERM signal, shutting down listener...'; lsnrctl stop" SIGTERM
 	trap "echo_red 'Caught SIGINT signal, shutting down listener...'; lsnrctl stop" SIGINT
-	tail -F -n 0 $listener_log | while read line; do echo -e "listener: $line"; done &
-	TAIL_LSNR_PID=$!
+	monitor $listener_log listener &
+	MON_LSNR_PID=$!
 	lsnrctl start
 	wait %1
 
@@ -25,13 +31,13 @@ elif [ "$1" = 'database' ]; then
 
 	start_db() {
 		echo_yellow "Starting listener..."
-		tail -F -n 0 $listener_log | while read line; do echo -e "listener: $line"; done &
+		monitor $listener_log listener &
 		lsnrctl start | while read line; do echo -e "lsnrctl: $line"; done
-		TAIL_LSNR_PID=$!
+		MON_LSNR_PID=$!
 		echo_yellow "Starting database..."
 		trap_db
-		tail -F -n 0 $alert_log | while read line; do echo -e "alertlog: $line"; done &
-		TAIL_ALERT_PID=$!
+		monitor $alert_log alertlog &
+		MON_ALERT_PID=$!
 		sqlplus / as sysdba <<-EOF |
 			pro Starting with pfile='$pfile' ...
 			startup pfile='$pfile';
@@ -39,29 +45,30 @@ elif [ "$1" = 'database' ]; then
 			exit 0
 		EOF
 		while read line; do echo -e "sqlplus: $line"; done
-		wait $TAIL_ALERT_PID
+		wait $MON_ALERT_PID
 	}
 
 	create_db() {
 		echo_yellow "Database does not exist. Creating database..."
 		date "+%F %T"
-		tail -F -n 0 $alert_log | while read line; do echo -e "alertlog: $line"; done &
-		TAIL_ALERT_PID=$!
-		tail -F -n 0 $listener_log | while read line; do echo -e "listener: $line"; done &
+		monitor $alert_log alertlog &
+		MON_ALERT_PID=$!
+		monitor $listener_log listener &
 		lsnrctl start | while read line; do echo -e "lsnrctl: $line"; done
-		TAIL_LSNR_PID=$!
+		MON_LSNR_PID=$!
 		/tmp/create_database.sh
 		echo_green "Database created."
 		date "+%F %T"
 		trap_db
-		wait $TAIL_ALERT_PID
+		wait $MON_ALERT_PID
 	}
 
 	stop() {
+        trap '' SIGINT SIGTERM
 		shu_immediate
 		echo_yellow "Shutting down listener..."
 		lsnrctl stop | while read line; do echo -e "lsnrctl: $line"; done
-		kill $TAIL_ALERT_PID $TAIL_LSNR_PID
+		kill $MON_ALERT_PID $MON_LSNR_PID
 		exit 0
 	}
 
