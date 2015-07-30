bf7cc0fbea6e82155caa9dacc0944bcdd7d29d62
bofm <bofm@noemail.com>
2015-07-30 18:49:35 +0300

    Added rlwrap install script

---
+++ step1/install_rlwrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install -y rlwrap
+
+# yum clean all
+# rm -rf /var/lib/{cache,log} /var/log/lastlog
