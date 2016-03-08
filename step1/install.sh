2984ee0a7eb6fd63a74afb7016922f3b257f93ce
bofm <bofm@github.com>
2016-03-08 18:48:38 +0300

    use env vars instead of hardcoded paths

--- step1/install.sh
+++ step1/install.sh
@@ -5,4 +5,4 @@ trap "echo '******* Caught SIGINT signal. Stopping...'; exit 2" SIGINT
 
 gosu oracle /bin/bash -c "cd /tmp/install/database/ && ./runInstaller -waitforcompletion -ignoreSysPrereqs -ignorePrereq -silent -noconfig -responseFile /tmp/install/db_install.rsp" &
 wait $!
-/app/oracle/product/12.1.0/dbhome_1/root.sh
+$ORACLE_HOME/root.sh
